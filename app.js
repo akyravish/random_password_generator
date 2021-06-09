4c6fb2bca02d83cc0f66fccde95e8aab9ac31239
akyravish <info.akshayravish@gmail.com>
2021-06-09 12:29:08 +0530

    import variable

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const randomFun = {
-	lower: randomLowerCase,
-	upper: randomUpperCase,
-	number: randomNumber,
-	symblo: randomSymbol,
-};
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const upperEl = document.getElementById('uppercase');
+const lowerEl = document.getElementById('lowercase');
+const symbolEl = document.getElementById('symbols');
+const numberEl = document.getElementById('numbers');
+const generateEl = document.getElementById('generate');
 
 const randomLowerCase = () => {
 	return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
@@ -22,4 +23,20 @@ const randomSymbol = () => {
 	return symbols[Math.floor(Math.random() * symbols.length)];
 };
 
-console.log(randomSymbol());
+const randomFun = {
+	lower: randomLowerCase,
+	upper: randomUpperCase,
+	number: randomNumber,
+	symble: randomSymbol,
+};
+
+const generatePass = (upper, lower, symbol, number, length) => {};
+
+generateEl.addEventListener('click', () => {
+	const length = +lengthEl.value;
+	const upper = upperEl.checked;
+	const lower = lowerEl.checked;
+	const symbol = symbolEl.checked;
+	const number = numberEl.checked;
+	generatePass(upper, lower, symbol, number, length);
+});
